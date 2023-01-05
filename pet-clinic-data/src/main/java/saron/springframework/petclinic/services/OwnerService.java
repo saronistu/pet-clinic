47e0a8932faee07bade0d9796c9e71e89cb62500
Saron <saron@drcarmen.com>
2023-01-05 16:17:53 +0200

    Add BaseEntity

---
+++ pet-clinic-data/src/main/java/saron/springframework/petclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package saron.springframework.petclinic.services;
+
+import saron.springframework.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
