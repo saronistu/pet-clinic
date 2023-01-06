e36c1aacc441da2bc1fa3ddf9c1153a1162ecade
Saron <saron@drcarmen.com>
2023-01-06 15:00:24 +0200

    Bootstrap PetType data

--- pet-clinic-web/src/main/java/saron/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/saron/springframework/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import saron.springframework.petclinic.model.Owner;
+import saron.springframework.petclinic.model.PetType;
 import saron.springframework.petclinic.model.Vet;
 import saron.springframework.petclinic.services.OwnerService;
+import saron.springframework.petclinic.services.PetTypeService;
 import saron.springframework.petclinic.services.VetService;
 
 @Component
@@ -14,9 +16,20 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
