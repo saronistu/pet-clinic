b47b7b894a492307438da8f173107993e929e2a4
Saron <saron@drcarmen.com>
2023-01-06 14:49:39 +0200

    Add resources and more entities

--- pet-clinic-data/src/main/java/saron/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/saron/springframework/petclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package saron.springframework.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
 }
