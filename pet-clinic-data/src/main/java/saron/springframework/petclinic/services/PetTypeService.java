5d88d39ddaacba6bf588e0420ef554d2f8f605c9
Saron <saron@drcarmen.com>
2023-01-06 14:56:50 +0200

    Add PetTypeService

---
+++ pet-clinic-data/src/main/java/saron/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package saron.springframework.petclinic.services;
+
+import saron.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
